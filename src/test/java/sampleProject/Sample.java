af3e3e00c56c524685ae55ca99d700b5b1dc9b5e
pavanmpr <pavan747413@gmail.com>
2023-11-20 13:32:27 +0000

    Initial Project

---
+++ src/test/java/sampleProject/Sample.java
@@ -0,0 +1,13 @@
+package sampleProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("www.google.com");
+		//System.out.println("Sample Testing");
+	}
+
+}
