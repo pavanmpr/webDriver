7ada2523fffd1f229b73b08984a7e14d2ff7cbe5
pavanmpr <pavan747413@gmail.com>
2023-11-20 14:03:07 +0000

    New Changes

--- src/test/java/sampleProject/Sample.java
+++ src/test/java/sampleProject/Sample.java
@@ -4,9 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample {
-	public static void main(String[] args) {
+	public static void main(String[]args) {
 		WebDriver driver=new ChromeDriver();
 		driver.get("www.google.com");
+		System.out.println("Test Sample");
 		//System.out.println("Sample Testing");
 	}
 
